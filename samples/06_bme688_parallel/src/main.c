ab4eef4a2739b626db1f8644e33c0fc8fa64213e
wass <wassim.filali@gmail.com>
2023-05-13 12:52:23 +0200

    fix end of sample detection

--- samples/06_bme688_parallel/src/main.c
+++ samples/06_bme688_parallel/src/main.c
@@ -34,22 +34,22 @@ void main(void)
 	//#define BME68X_HEAT_STAB_MSK                      UINT8_C(0x10)
 	printf("0xA0 => nhs: No Heat Stability\n");
 	printf("sample:count (meas index/gas index) : Temperature°, Pressure Pa, Humidity %%, 'Gas resistance ohm'\n");
+	uint8_t last_index = 1;
 	while (1) {
 		//bme688 API usage, sensor_sample_fetch and sensor_channel_get also available
 		bme688_sample_fetch(dev,SENSOR_CHAN_ALL);
 		struct bme68x_data data[10];
 		uint8_t n_fields = bme688_data_get(dev, data);
-		uint8_t last_index = 1;
 		if(n_fields != 0){
 			for(uint8_t i = 0; i < n_fields; i++){
+				if(data[i].gas_index < last_index){
+					printf("---\n");//this is the time to broadcast the previous group
+					measure_count++;
+				}
+				last_index = data[i].gas_index;
 				if ((data[i].status & BME68X_NEW_DATA_MSK) &&
 					(data[i].status & BME68X_GASM_VALID_MSK)
 					){
-						if(data[i].gas_index < last_index){
-							printf("---\n");//this is the time to broadcast the previous group
-							measure_count++;
-							last_index = data[i].gas_index;
-						}
 						printf("sample:%d (%u/%d) : %.2f° , %.2f Pa , %.2f %% , %s %.2f ohm\n",
 							measure_count,
 							data[i].meas_index,
